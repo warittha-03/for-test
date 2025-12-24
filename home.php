eee1b5ad67f3230b86b693c4e7b535cb135facb3
warittha-03 <warittha.o@rmutsvmail.com>
2025-12-24 13:22:11 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,33 @@
+<body bgcolor="#CC9900">
+	<table width="1238" height="530" border="0">
+		<tr>
+			<td width="23" rowspan="2">&nbsp;</td>
+			<td height="119" colspan="2" bgcolor="#006600">
+				<h1><center><font color="#FFFFFF">พรชัยอพาร์ตเมนท์</font></center></h1>
+				<?php include "Date.php";?>
+			</td>
+			<td width="43" rowspan="2">&nbsp;</td>
+		</tr>
+		<tr>
+			<td width="1007" bgcolor="#FFFFFF">
+				<p>
+					อพาร์ทเมนต์ หรู สภาพแวดล้อมดี ปลอดภัย บริการทั้งห้องพัก รายวัน รายเดือน
+					เหมาะสำหรับ <br> นักศึกษา ใกล้แหล่งชุมชน และ ห้างสรรพสินค้า BigC , Lotus
+				<p>
+					ห้องพักทันสมัย เฟอร์นิเจอร์อย่างดี และสุขภัณฑ์ครบครัน มีสิ่งอำนวยความสะดวกมากมาย
+					อาทิ WIFI อินเทอร์เน็ตความเร็วสูงทุกห้อง มีพัดลม และเครื่องปรับอากาศให้เลือก มีทีวี
+					คอมพิวเตอร์ให้เช่า
+				<p>
+					เชิญชมห้องตัวอย่าง ได้ทุกวัน <a href=amit.php>ติดต่อสอบถาม</a>	
+			</td>
+			<td width="190" bgcolor="#FFFF66"><ul>
+				<li><a href="home.php">หน้าแรก</li>
+				<li><a href="room.php">ห้องพัก&ราคา</li>
+				<li><a herf="guest.php">สมุดเยี่ยม</li>
+				<li><a href="contact.php">ติดต่ออออออ</li>
+				</ul><p>
+				<?php include "login_form.php";?>
+			</td>
+		</tr>
+	</table>
+<body>
